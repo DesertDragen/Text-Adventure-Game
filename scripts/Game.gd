adc8af999b2f0df600fce14a95adaac6a8b076e1
Amezaiku <52254100+DesertDragen@users.noreply.github.com>
2023-08-14 14:11:50 -0400

    Added code to handle user input

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,5 +1,14 @@
 extends Control
 
+# Storing the data of the InputResponse scene
+const InputResponse = preload("res://scenes/InputResponse.tscn")
+
+# Only use $ to access the children of the nodes
+onready var history_rows = $Background/MarginContainer/Rows/GameInfo/ScrollContainer/HistoryRows
+
 
 func _on_Input_text_entered(new_text: String) -> void:
-	print(new_text)
+	# Creating an instance of the InputResponse scene
+	var input_response = InputResponse.instance()
+	# Adding the instance of the scene as a child of the history_rows node
+	history_rows.add_child(input_response)
