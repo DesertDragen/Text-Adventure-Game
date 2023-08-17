69c4cb957c0e59d3fb0aa73bb29d1acd7190c5d7
Amezaiku <52254100+DesertDragen@users.noreply.github.com>
2023-08-17 19:47:14 -0400

    Added auto scroll

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -3,10 +3,28 @@ extends Control
 # Storing the data of the InputResponse scene
 const InputResponse = preload("res://scenes/InputResponse.tscn")
 
+var max_scroll_length := 0
+
 # Only use $ to access the children of the nodes
-onready var history_rows = $Background/MarginContainer/Rows/GameInfo/ScrollContainer/HistoryRows
+onready var history_rows = $Background/MarginContainer/Rows/GameInfo/Scroll/HistoryRows
+onready var scroll = $Background/MarginContainer/Rows/GameInfo/Scroll
+onready var scrollbar = scroll.get_v_scrollbar()
 
 
+func _ready() -> void:
+	scrollbar.connect("changed", self, "handle_scrollbar_changed")
+	# Set a max baseline for the scroll length
+	max_scroll_length = scrollbar.max_value
+	
+
+# Only scroll down when the max value of the scrollbar changes
+func handle_scrollbar_changed():
+	if max_scroll_length != scrollbar.max_value:
+		max_scroll_length = scrollbar.max_value
+		scroll.scroll_vertical = max_scroll_length
+
+
+# 
 func _on_Input_text_entered(new_text: String) -> void:
 	# Creating an instance of the InputResponse scene
 	var input_response = InputResponse.instance()
