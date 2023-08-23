f58910a0847e86e5f9e74077a4a631e27c029a86
Amezaiku <52254100+DesertDragen@users.noreply.github.com>
2023-08-23 19:04:59 -0400

    Added a start message

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,6 +2,7 @@ extends Control
 
 # Storing the data of the InputResponse scene
 const InputResponse = preload("res://scenes/InputResponse.tscn")
+const Response = preload("res://scenes/Response.tscn")
 
 # This value can be edited in the editor
 export (int) var max_lines_remembered := 30
@@ -19,6 +20,9 @@ func _ready() -> void:
 	scrollbar.connect("changed", self, "handle_scrollbar_changed")
 	# Set a max baseline for the scroll length
 	max_scroll_length = scrollbar.max_value
+	var starting_message = Response.instance()
+	starting_message.text = "You spawn into what seems to be a dungeon of some sort. You remember talking to a God about where you wanted to go. You want to leave the dungeon and see the sights of the world. You can type 'help' to see your available commands."
+	add_response_to_game(starting_message)
 	
 
 # Only scroll down when the max value of the scrollbar changes
@@ -39,9 +43,13 @@ func _on_Input_text_entered(new_text: String) -> void:
 	var response = command_processor.process_command(new_text)
 	# Sending the new_text to the function in InputResponse script
 	input_response.set_text(new_text, response)
-	# Adding the instance of the scene as a child of the history_rows node
-	history_rows.add_child(input_response)
 	
+	add_response_to_game(input_response)
+
+
+func add_response_to_game(response: Control):
+	# Adding the instance of the scene as a child of the history_rows node
+	history_rows.add_child(response)
 	delete_history_beyond_limit()
 
 
